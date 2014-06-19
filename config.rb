73edb2533193fd51542129a0f5fa9343218535c8
Pablo Oldani <oldani.pablo@gmail.com>
2014-06-19 09:42:05 -0300

    Initial commit

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/rebotic/"
+http_images_path = "/rebotic/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/rebotic/fonts"
+css_dir = "public/rebotic/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
